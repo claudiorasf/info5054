f16fe8b05ec5ae8b3cfafbe62cfc44ed41469fb6
Claudio Souza <claudio.rasf@gmail.com>
2020-01-23 19:58:06 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//create our first component
+class Test extends React.Component {
+  render() {
+    return <h1>Hello World...!</h1>
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class MyPictures extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+      id: null
+    };
+  }//constructor
+
+  componentDidMount() {
+    fetch('https://picsum.photos/v2/list')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        //handle errors
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }//componentDidMount
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>
+    } else if (!isLoaded) {
+      return <div>Loading...</div>
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              {item.author}
+              {item.url}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+
+}//MyPictures
+
+ReactDOM.render(<Test />, document.getElementById('root'));
+ReactDOM.render(<MyPictures />, document.getElementById('root'));
